f959a9bb20c91925b45dbacfaf90c09b1786b44e
Arthur Zorzo <arthurcorteszorzo@gmail.com>
2023-07-19 11:05:11 -0300

    find jogador by id

--- src/main/java/com/zorzoarthur/corinthians/services/JogadoresService.java
+++ src/main/java/com/zorzoarthur/corinthians/services/JogadoresService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.zorzoarthur.corinthians.entities.Jogadores;
 import com.zorzoarthur.corinthians.dto.JogadoresDTO;
@@ -15,9 +17,16 @@ public class JogadoresService {
     @Autowired
     private JogadoresRepository jogadoresRepository;
 
-    public List<JogadoresDTO> findAll(){
+    @Transactional(readOnly = true)
+    public List<JogadoresDTO> findAll() {
         List<Jogadores> result = jogadoresRepository.findAll();
         List<JogadoresDTO> dto = result.stream().map(x -> new JogadoresDTO(x)).toList();
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public JogadoresDTO findById(@PathVariable Long id) {
+        Jogadores jogador = jogadoresRepository.findById(id).get();
+        return new JogadoresDTO(jogador);
     }
 }
