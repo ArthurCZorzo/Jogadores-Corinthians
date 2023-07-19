8326583696763fd5eddf08b3d22fec2f3947ad3c
Arthur Zorzo <arthurcorteszorzo@gmail.com>
2023-07-19 02:06:44 -0300

    Projeto Criado

---
+++ src/test/java/com/zorzoarthur/corinthians/CorinthiansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zorzoarthur.corinthians;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorinthiansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
